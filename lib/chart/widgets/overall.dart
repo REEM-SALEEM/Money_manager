11f91a0cd00f19bef91dcd81c3b9dd0d910bb982
Reem Saleem Hussaim M.T <reemsaleem.mt@gmail.com>
2022-11-21 10:52:26 +0530

    first commit

--- lib/chart/widgets/overall.dart
+++ lib/chart/widgets/overall.dart
@@ -17,7 +17,6 @@ List<String> items = ['All', 'Today', 'Yesterday'];
 class _ScreenOverallState extends State<ScreenOverall> {
   @override
   void initState() {
-    TransactionDB.instance.filterListNotifier;
     TransactionDB.instance.transactionListNotifier;
     TransactionDB.instance.refresh();
     setState(() {});
@@ -32,48 +31,6 @@ class _ScreenOverallState extends State<ScreenOverall> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 41, 42, 41),
       body: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            color: Colors.lightGreen,
-            child: DropdownButton<String>(
-              underline: Column(),
-              //Initially 'All'
-              value: dropdownValue,
-              onChanged: (String? value) async {
-               
-                setState(
-                  () {
-                    dropdownValue = value!;
-                    if (dropdownValue == 'Today') {
-                      chartsort(
-                          TransactionDB.instance.transactionListNotifier.value,
-                          'Today');
-                    } else if (dropdownValue == 'Yesterday') {
-                      chartsort(
-                          TransactionDB.instance.transactionListNotifier.value,
-                          'Yesterday');
-                    }
-                    
-                   
-                  },
-                );
-                setState(() {  TransactionDB.instance.transactionListNotifier.value.clear();
-                TransactionDB.instance.filterListNotifier.value.clear();
-                       TransactionDB.instance.refresh();});
-              },
-              items: items.map<DropdownMenuItem<String>>((items) {
-                return DropdownMenuItem(
-                  value: items,
-                  child: Text(
-                    " $items",
-                    style: const TextStyle(fontWeight: FontWeight.w900),
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-        ),
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 08),
           child: ValueListenableBuilder(
@@ -115,76 +72,28 @@ List<Overall> chartsort(List<TransactionModel> model, String dropdownValue) {
   CategoryType categorytype;
   List visited = [];
   List<Overall> newData = [];
-  DateTime now = DateTime.now();
-  final seltod = DateTime(now.year, now.month, now.day);
-  final selyes = DateTime(now.year, now.month, now.day - 1);
-  // DateTime? selected = DateTime.now();
 
 //length of array cant be zero.
   for (var i = 0; i < model.length; i++) {
     visited.add(0);
   }
-  if (dropdownValue == 'All') {
-    for (var i = 0; i < model.length; i++) {
-      value = model[i].amount;
-      categorytype = model[i].category.type;
 
-      for (var j = i + 1; j < model.length; j++) {
-        if (model[i].category.type == model[j].category.type) {
-          value = value + model[j].amount;
-          visited[j] = -1;
-        }
-      }
+  for (var i = 0; i < model.length; i++) {
+    value = model[i].amount;
+    categorytype = model[i].category.type;
 
-      if (visited[i] != -1) {
-        newData.add(Overall(
-          type: categorytype.name.toUpperCase(),
-          amount: value,
-        ));
+    for (var j = i + 1; j < model.length; j++) {
+      if (model[i].category.type == model[j].category.type) {
+        value = value + model[j].amount;
+        visited[j] = -1;
       }
     }
-  } else if (dropdownValue == 'Yesterday') {
-    for (var i = 0; i < model.length; i++) {
-      if (model[i].date == selyes) {
-        value = model[i].amount;
-        categorytype = model[i].category.type;
 
-        for (var j = i + 1; j < model.length; j++) {
-          if (model[i].category.type == model[j].category.type) {
-            value = value + model[j].amount;
-            visited[j] = -1;
-          }
-        }
-
-        if (visited[i] != -1) {
-          newData.add(Overall(
-            type: categorytype.name.toUpperCase(),
-            amount: value,
-          ));
-        }
-      }
-    }
-  } else if (dropdownValue == 'Today') {
-    for (var i = 0; i < model.length; i++) {
-      if (model[i].date.day == seltod.day) {
-        value = model[i].amount;
-        categorytype = model[i].category.type;
-
-        for (var j = i + 1; j < model.length; j++) {
-          if (model[i].category.type == model[j].category.type &&
-              model[i].date.day == model[j].date.day) {
-            value = value + model[j].amount;
-            visited[j] = -1;
-          }
-        }
-
-        if (visited[i] != -1) {
-          newData.add(Overall(
-            type: categorytype.name.toUpperCase(),
-            amount: value,
-          ));
-        }
-      }
+    if (visited[i] != -1) {
+      newData.add(Overall(
+        type: categorytype.name.toUpperCase(),
+        amount: value,
+      ));
     }
   }
   return newData;
