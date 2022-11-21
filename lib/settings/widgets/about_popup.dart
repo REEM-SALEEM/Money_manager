d7a6f277e41dc6a6bf8900a73b6c9955c73f1c39
Reem Saleem Hussaim M.T <reemsaleem.mt@gmail.com>
2022-11-21 12:50:21 +0530

    first commit

--- lib/settings/widgets/about_popup.dart
+++ lib/settings/widgets/about_popup.dart
@@ -4,33 +4,69 @@ Future<void> aboutPopup(BuildContext context) async {
   showDialog(
     context: context,
     builder: ((context) {
-      return SimpleDialog(
-          backgroundColor: Colors.black,
-          title: const Text(
-            'ABOUT',
-            style: TextStyle(color: Colors.white),
-          ),
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-                style: TextStyle(color: Colors.white),
+      return SimpleDialog(backgroundColor: Colors.black, children: [
+        Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          const SizedBox(width: 16),
+          SizedBox(
+            width: 65,
+            height: 65,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(
+                'assets/images/1.png',
+                fit: BoxFit.fill,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(180, 0, 0, 0),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(context);
-                },
-                child: const Text(
-                  'Go Back',
-                  style: TextStyle(color: Colors.white),
-                ),
+          ),
+          const SizedBox(width: 16),
+          Column(
+            children: [
+              const Text(
+                ' Croupier',
+                style: TextStyle(color: Colors.white, fontSize: 24),
               ),
-            )
-          ]);
+              const SizedBox(height: 3),
+              // ignore: prefer_const_constructors
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: const Center(
+                    child: Text(
+                  'Version 1.0.1',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+              ),
+              const SizedBox(height: 15),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.copyright_outlined,
+                    color: Colors.grey,
+                    size: 15,
+                  ),
+                  Text(
+                    ' 2022 company',
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              ),
+            ],
+          )
+        ]),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(180, 17, 0, 0),
+          child: TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(context);
+            },
+            child: const Text(
+              'Go Back',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        )
+      ]);
     }),
   );
 }
